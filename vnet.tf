2a78e71b7e8b7f402ab6a8ec0a5b7d9661c54935
Paul Jones <9488971+paulm-jones@users.noreply.github.com>
2021-05-13 18:51:37 +0000

    Fix Resource Group name

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/PaulJonesCHIP-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "pauljones-chip-rg"
+  resource_group_name = azurerm_resource_group.myresourcegroup.resource_group_name
 }
